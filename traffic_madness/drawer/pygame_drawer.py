b9e715b62c630e3d148da4a844fb9e3db855edad
whaibop <dakarlss@student.chalmers.se>
2017-11-21 11:42:16 +0100

    Not tested drawer

--- traffic_madness/drawer/pygame_drawer.py
+++ traffic_madness/drawer/pygame_drawer.py
@@ -1,5 +1,33 @@
+import pygame
 from traffic_madness.drawer import Drawer
 
 
 class PyGameDrawer(Drawer):
-    pass
+
+    def __init__(self, resolution, title, track):
+        pygame.init()
+        self.track = track
+        
+        SCREEN_RESOLUTION = resolution
+        self.screen = pygame.display.set_mode(resolution)
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock()
+
+        self.background_surface = pygame.Surface(screen.get_size())
+        self.background_surface = backgroun_surface.convert()
+        BG_COLOR = pygame.color(255,255,255,255)
+        self.background_surface.fill(BG_COLOR)
+
+        self.screen.blit(background_surface, (0, 0))
+
+        self.rect_size = 5
+
+        pygame.display.flip()
+
+    def update(car_positions):
+        car_rects = [pygame.Rect(pos, 350, self.rect_size, self.rect_size)
+                     for pos in car_positions]
+        rect_surfaces = [pygame.Surface(rect.size)
+                         for rect in car_rects]
+        
+        
--- traffic_madness/main.py
+++ traffic_madness/main.py
@@ -1,3 +1,4 @@
+import pygame
 from traffic_madness.drawer.pygame_drawer import PyGameDrawer
 from traffic_madness.track.single_lane_track import SingleLaneTrack
 
@@ -5,10 +6,11 @@ from traffic_madness.track.single_lane_track import SingleLaneTrack
 def run_simulation():
     track = SingleLaneTrack(speed_limit=10, track_length=1000,
                             max_num_cars=100)
-    drawer = PyGameDrawer()
+    # Add to drawer, make max_num_cars known to drawer so we can have a fixed number of rects and use selective blit instead of the whole screen.
+    drawer = PyGameDrawer((720, 720), "Traffic Madness Simulation", track)
     while True:
         track.update()
-        drawer.update(track.get_car_positions())
+        #drawer.update(track.get_car_positions())
 
 
 if __name__ == '__main__':
