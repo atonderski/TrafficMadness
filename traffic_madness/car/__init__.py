a441206f56bb311021b3f0bde88c50ac769e1a5a
Unknown <tfinke95@gmail.com>
2017-11-21 10:48:35 +0100

    Corrected spelling mistake

--- traffic_madness/car/__init__.py
+++ traffic_madness/car/__init__.py
@@ -2,7 +2,9 @@
 class Car:
     def __init__(self, position, velocity):
         self.position = position
-        self.velocty = velocity
+        self.velocity = velocity
 
     def update(self, target_speed, nearby_cars):
+        timestep = 1
+        self.position += self.velocty * timestep
         pass
