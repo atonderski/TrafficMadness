d77e1ff76ea774ca5200373ea15eedaabaae69d0
Unknown <tfinke95@gmail.com>
2017-11-21 11:35:54 +0100

    Added acceleration as property of a car

--- traffic_madness/car/__init__.py
+++ traffic_madness/car/__init__.py
@@ -1,10 +1,9 @@
 
 class Car:
-    def __init__(self, position, velocity):
+    def __init__(self, position, velocity, acceleration):
         self.position = position
         self.velocity = velocity
+        self.acceleration = acceleration # Add capability of acceleration
 
     def update(self, target_speed, nearby_cars):
-        timestep = 1
-        self.position += self.velocty * timestep
         pass
