c529bc6cd9eff738946b02232b54a003aa483781
Unknown <tfinke95@gmail.com>
2017-11-27 10:36:09 +0100

    Added lane attribute to cars, changed safety distance in simple car
    Realistic values for acceleration and deceleration. Safety distance set to 1.8 * velocity

--- traffic_madness/car/__init__.py
+++ traffic_madness/car/__init__.py
@@ -1,8 +1,10 @@
 class Car:
-    def __init__(self, position, velocity, acceleration):
+    def __init__(self, position, lane, velocity, acceleration):
         self.position = position
+        self.lane = lane
         self.velocity = velocity
-        self.acceleration = acceleration # Add capability of acceleration
+        self.acceleration = 4.3 # Add capability of acceleration
+        self.deceleration = 7.5
 
     def update(self, target_speed, nearby_cars):
         pass
