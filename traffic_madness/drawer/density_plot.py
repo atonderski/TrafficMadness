b97e39da478822d5bc268cf1189381e6fc187d4d
Fredrik <fredrik@localhost.localdomain>
2017-11-28 15:15:29 +0100

    add prototype of binning density plot

--- traffic_madness/drawer/density_plot.py
+++ traffic_madness/drawer/density_plot.py
@@ -1,4 +1,37 @@
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
+import numpy as np
+
+def __init(self,resolution,track):
+    self.track = track
+    self.resolution = resolution
+
+alist = np.array([1,7,12,25,26])
+
+def find_nearest(array,value):
+    idx = (np.abs(array-value)).argmin()
+    return array[idx]
+      
+def density(car_positions, track_length,resolution):
+    X = np.linspace(0,track_length,resolution)
+    Y = np.zeros(resolution)
+    print(X)
+    print(Y)
+    for i in range(len(car_positions)):
+        print(car_positions[i])
+        nearest = find_nearest(X,car_positions[i])
+        idx = list(X).index(nearest)
+        Y[idx] = Y[idx] +1
+        print(Y)
+        norm = np.linalg.norm(Y)
+        if (norm != 0):
+            Y = Y / norm
+    return Y
+
+def showdensity(density):
+    plt.plot(list(density))
+    plt.ylabel('density')
+    plt.show()
+
+dens = density(alist,30,5)
+showdensity(dens)
+    
