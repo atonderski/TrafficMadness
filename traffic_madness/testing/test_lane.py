b98c3102d3af93970ec8d5a1ed23d69c1b173b7e
Adam Tonderski <atonderski@gmail.com>
2017-11-30 12:40:56 +0100

    More work on multi lane
    Also updated pipfile to work with python 3 instead

--- traffic_madness/testing/test_lane.py
+++ traffic_madness/testing/test_lane.py
@@ -1,18 +1,23 @@
-from traffic_madness.track.trackbucket import TrackBucket
-from traffic_madness.car import Car
 import pytest
 
+from traffic_madness.car import Car
+from traffic_madness.track.trackbucket import TrackBucket
+
+
 def test_default():
-    trackbucket = TrackBucket(100,25, 3)
+    trackbucket = TrackBucket(100, 25, 3)
     assert len(trackbucket.bucket_list) == 4
+
 
 def test_raise_not_divisible():
     with pytest.raises(ValueError):
         trackbucket = TrackBucket(99, 30, 3)
 
+
 def test_add_car():
-    trackbucket = TrackBucket(100,25, 1)
-    trackbucket.add_car(Car(60, 20 , 20))
+    trackbucket = TrackBucket(100, 25, 1)
+    trackbucket.add_car(Car(60, 20, 20))
+    print(trackbucket.bucket_list)
     assert len(trackbucket.bucket_list[0]) == 0
     assert len(trackbucket.bucket_list[1]) == 0
     assert len(trackbucket.bucket_list[2]) == 1
@@ -29,3 +34,39 @@ def test_car_has_moved():
     assert len(trackbucket.bucket_list[1]) == 0
     assert len(trackbucket.bucket_list[2]) == 0
     assert len(trackbucket.bucket_list[3]) == 0
+
+def test_get_nearby_cars():
+    trackbucket = TrackBucket(100, 25, 2)
+
+    c1 = Car(0, 20, 20, 0)
+    c2 = Car(60, 20, 20, 0)
+    c3 = Car(60, 20, 20, 1)
+    c4 = Car(50, 20, 20, 0)
+    trackbucket.add_car(c1)
+    trackbucket.add_car(c2)
+    trackbucket.add_car(c3)
+    trackbucket.add_car(c4)
+
+    nearby_cars = trackbucket.get_nearby_cars(55)
+    print(nearby_cars)
+    assert c1 not in nearby_cars[0] and c1 not in nearby_cars[1]
+    assert c2 in nearby_cars[0] and c2 not in nearby_cars[1]
+    assert c3 in nearby_cars[1] and c3 not in nearby_cars[0]
+    assert c4 in nearby_cars[0] and c4 not in nearby_cars[1]
+
+def test_get_all_cars():
+    trackbucket = TrackBucket(100, 25, 2)
+
+    c1 = Car(0, 20, 20, 0)
+    c2 = Car(60, 20, 20, 0)
+    c3 = Car(60, 20, 20, 1)
+    c4 = Car(50, 20, 20, 0)
+    trackbucket.add_car(c1)
+    trackbucket.add_car(c2)
+    trackbucket.add_car(c3)
+    trackbucket.add_car(c4)
+
+    all_cars = trackbucket.get_all_cars()
+    assert c1 in all_cars and c2 in all_cars and c3 in all_cars and c4 in all_cars
+    assert len(all_cars) == 4
+    assert trackbucket.get_num_cars() == 4
--- traffic_madness/track/trackbucket.py
+++ traffic_madness/track/trackbucket.py
@@ -1,54 +1,78 @@
-from traffic_madness.car.simple_car import SimpleCar
+import itertools
+
 from traffic_madness.car.aggressive_car import AggressiveCar
+from traffic_madness.car.simple_car import SimpleCar
 from traffic_madness.config import Config
+
 
 class TrackBucket():
     """ Splits the track into buckets with each bucket containing the
         cars with positions in the interval of the bucket."""
 
     def __init__(self, track_length, bucket_length, num_lanes):
-        """Create a bucket set. Track length need to be divisible by bucket length. """
+        """Create a bucket set. Track length need to be divisible by bucket
+        length. """
         self.track_length = track_length
         self.bucket_length = bucket_length
         self.num_lanes = num_lanes
-        n_buckets = track_length/bucket_length
+        n_buckets = float(track_length) / bucket_length
         self.bucket_list = []
-        if n_buckets % 1 == 0:
-            for i in range(int(n_buckets)):
+        if int(n_buckets) == n_buckets:
+            for _ in range(int(n_buckets)):
                 self.bucket_list.append([])
         else:
-            raise ValueError("Track length and bucket length not integer divisible")
+            raise ValueError(
+                "Track length and bucket length not integer divisible")
+
+    def _get_index_from_position(self, position):
+        return int(position / self.bucket_length)
 
     def add_car(self, car):
         """ Adds the car to the appropriate bucket depending on its position"""
-        bucket_index = int(car.position/self.bucket_length)
+        bucket_index = self._get_index_from_position(car.position)
         self.bucket_list[bucket_index].append(car)
 
     def car_has_moved(self, car, old_position):
         """ Check whether the car's new position belong to a new bucket.
             If it does, it is moved and is removed from its old bucket."""
-        old_bucket_index = self._get_index_from_position(old_position)  
-        new_bucket_index = self._get_index_from_position(new_position)
+        old_bucket_index = self._get_index_from_position(old_position)
+        new_bucket_index = self._get_index_from_position(car.position)
 
         if old_bucket_index == new_bucket_index:
             return
         else:
             self.bucket_list[old_bucket_index].remove(car)
             self.bucket_list[new_bucket_index].append(car)
-            
-    def get_num_cars():
+
+    def get_num_cars(self):
         """ Returns the total sum of cars active in all buckets."""
+        return sum([len(bucket) for bucket in self.bucket_list])
 
-    def get_all_cars():
-        """ Returns an unordered list of car objects for all cars 
+    def get_all_cars(self):
+        """ Returns an unordered list of car objects for all cars
             active in all buckets."""
+        return list(itertools.chain.from_iterable(self.bucket_list))
 
-    def get_nearby(self, position):
+    def get_nearby_cars(self, position):
         """ Returns an unordered list of car objects for cars in
             (i) The bucket the position maps to
             (ii) The next bucket from the position
             (iii) The previous bucket from the position.
-            
+
             Wrapping is taken into account, so if the positions bucket is the
             last bucket, the first bucket is returned as (iii)."""
+        curr_ind = self._get_index_from_position(position)
+        prev_ind = curr_ind - 1
+        next_ind = (curr_ind + 1) % len(self.bucket_list)
 
+        nearby_cars = []
+        for lane in range(self.num_lanes):
+            cars_in_lane = []
+            for bucket_ind in [prev_ind, curr_ind, next_ind]:
+                cars_in_bucket_and_lane = [
+                    car for car in self.bucket_list[bucket_ind] if
+                    car.lane == lane]
+                cars_in_bucket_and_lane.sort(key=lambda x: x.position)
+                cars_in_lane += cars_in_bucket_and_lane
+            nearby_cars.append(cars_in_lane)
+        return nearby_cars
