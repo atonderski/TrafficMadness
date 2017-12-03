c99751fa80865a0789555982394e1b5ec192864a
Unknown <tfinke95@gmail.com>
2017-12-03 22:25:22 +0100

    Added equilibration and observation
    Devide simulation into 3 parts: Spawning, equilibration and observation. First version that works. Should maybe be written more nicely, but can be deleted quite easily

--- traffic_madness/config.py
+++ traffic_madness/config.py
@@ -2,22 +2,24 @@
 of the simulation'''
 class Config():
     def __init__(self):
-        self.timestep = 0.1
+        self.timestep = 0.5
+        self.equilibration = 120 # Set equilibration time
+        self.observation = 180 # Set time for observation
         # track properties
-        self.aggressives = 0.1 # % of aggressive drivers
-        self.passives = 0.25 # passive drivers
-        self.lanes = 5
+        self.aggressives = 1.0 # % of aggressive drivers
+        self.passives = 0.0 # passive drivers
+        self.lanes = 3
         self.speed_limit = 20
         self.track_length = 1000
-        self.buckets = float(66)
+        self.buckets = 66.0
         self.max_num_cars = 150
-        self.buffer_length = 500
+        self.buffer_length = 10
         self.bucket_length = self.track_length / self.buckets
         # car properties
         self.acceleration = 4.3
         self.deceleration = 7.5
         self.aggressiveness = 1.2
-        self.passiveness = 0.5
-        self.safetymultiplier = 1.0
+        self.passiveness = 0.8
+        self.safetymultiplier = 1.8
         self.car_length = 5.0
         
--- traffic_madness/main.py
+++ traffic_madness/main.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import time
 
 from traffic_madness.config import Config
 from traffic_madness.drawer.pygame_drawer import PyGameDrawer
@@ -9,7 +10,7 @@ from traffic_madness.observables.traffic_flow import traffic_flow
 
 def run_simulation():
     config = Config()
-    time = 0
+    time_counter = 0
     track = MultiLaneTrack(speed_limit=config.speed_limit,
                            track_length=config.track_length,
                            num_lanes=config.lanes,
@@ -18,15 +19,49 @@ def run_simulation():
     # number of rects and use selective blit instead of the whole screen.
     drawer = PyGameDrawer((1000, 1000), "Traffic Madness Simulation", track)
 
-    # Define an array for averaging the traffic flow (now 20 timesteps average)
-    flow_array = np.zeros(20)
-    while True:
+    # Define an array for averaging the traffic flow (now 1 min average)
+    # equilibration needs to be at least the average time
+    flow_array = np.zeros(int(60 / config.timestep))
+    run = True
+    start = time.time()
+    while run:
         track.update()
-        time += 1
+        time_counter += 1
         # Get flow and updated flow array
         flow, flow_array = traffic_flow(track.get_flow_cars(), flow_array)
-        # Give flow to the drawer to draw it
-        drawer.update(track.get_all_cars(), time, flow)
+        # # Give flow to the drawer to draw it
+        # drawer.update(track.get_all_cars(), time_counter, flow)
+
+        if len(track.get_all_cars()) == config.max_num_cars:
+            end = time.time()
+            print('Spawning done, time %f s' % (end - start))
+            equilibration = config.equilibration
+            observation = config.observation
+            time_counter = 0
+            for i in range(0, int(equilibration / config.timestep)):
+                track.update()
+                time_counter += 1
+                # Get flow and updated flow array
+                flow, flow_array = traffic_flow(track.get_flow_cars(), flow_array)
+                # # Give flow to the drawer to draw it
+                # drawer.update(track.get_all_cars(), time_counter, flow)
+            end = time.time()
+            print('Equilibration done, time %f s' % (end - start))
+            time_counter = 0
+            file = open('data/flow_aggressiveness%.2f_cars%d.dat' %
+                        (config.aggressives, config.max_num_cars), 'w')
+            for j in range(0, int(observation / config.timestep)):
+                track.update()
+                time_counter += 1
+                # Get flow and updated flow array
+                flow, flow_array = traffic_flow(track.get_flow_cars(), flow_array)
+                file.write('%f \t %f \n' % (time_counter * config.timestep, flow))
+                # Give flow to the drawer to draw it
+                drawer.update(track.get_all_cars(), time_counter, flow)
+            file.close()
+            end = time.time()
+            print('Observation done, time %f s' % (end - start))
+            run = False
 
 
 if __name__ == '__main__':
