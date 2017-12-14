94b489c3feac9e21a95f2804672de8dbf425e3cf
Fredrik Johnsson <frejohns@remote11.chalmers.se>
2017-12-14 12:27:55 +0100

    fix phase transition plot

--- traffic_madness/plot_phase_transition.py
+++ traffic_madness/plot_phase_transition.py
@@ -6,8 +6,9 @@ from os import listdir
 import re
 from mpl_toolkits import mplot3d
 from matplotlib import cm
+from mpl_toolkits.mplot3d import axes3d, Axes3D #<-- Note the capitalization! 
 
-data = np.loadtxt('data/phases/phase-transition_3.dat',delimiter='\t',usecols=range(20))
+data = np.loadtxt('data/phases/phase-transition_2.dat',delimiter='\t',usecols=range(20))
 print(data)
 
 xlen=len(data[1,:])
@@ -16,6 +17,9 @@ Xs = range(xlen)
 Ys = range(ylen)
 Xa, Ys = np.meshgrid(Xs, Ys)
 fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot_surface(Xs,Ys,data, cmap=cm.coolwarm)
+#ax = plt.axes(projection='3d')
+ax = Axes3D(fig)
+ax.plot_surface(Xs,Ys,data,cmap=cm.coolwarm)
+#ax.set_xlabel('xlabel')
+#ax.set_ylabel('ylabel')
 plt.show()
